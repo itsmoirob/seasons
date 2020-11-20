0a14ae77350782e8b752df403a6477a016c2d672
Robbie <rob.w.sherman@gmail.com>
2020-11-20 06:56:10 +0000

    Add some style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
-import { getRoles } from '@testing-library/react';
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+  summer: {
+    pageText: 'It\'s summer, let\'s hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    pageText: 'It\'s winter, brrr it\'s cold',
+    iconName: 'snowflake'
+  }
+}
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -11,14 +22,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const pageText = season === 'summer' ? 'To the beach' : 'Brrr, it is chilly';
-  const icon = season === 'summer' ? 'sun' : 'snowflake';
+  const { pageText, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${icon} icon`}></i>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
       <h1>{pageText}</h1>
-      <i className={`${icon} icon`}></i>
+      <i className={`icon-right massive ${iconName} icon`}></i>
     </div>
   )
 };
